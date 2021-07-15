59cf7ce139cf670cb903ce58ebf52e1267f3541f
mattblasa <blasa.matthew@yahoo.com>
2021-07-14 21:50:15 -0500

    Update product_table_create.sql

--- Marketing Data/product_table_create.sql
+++ Marketing Data/product_table_create.sql
@@ -1,5 +1,10 @@
 SELECT *
-FROM public.opportunities_data as od
+FROM public.revenue_raw_data as r
 LEFT JOIN public.marketing_dat_clean as mc
-ON 
-LEFT Sdsd
+ON r.account_no = mc.account_no
+LEFT JOIN public.opportunities_data as od
+ON r.account_no = CAST(od.new_account_no, int
+LEFT JOIN public.targets_raw_data as t
+ON r.account_no = t.account_no
+LIMIT 100
+--Need to join on ids. Redo this. 
